a3fc2f1b9d32a3bef9ea7003506398254fc0e590
Michael Matloka <dev@twixes.com>
2021-05-05 22:04:18 +0200

    Set GeoIP data on person with $set and $set_once

--- index.test.ts
+++ index.test.ts
@@ -35,6 +35,38 @@ test('event is enriched with IP location', async () => {
     )
 })
 
+test('person is enriched with IP location', async () => {
+    const event = await processEvent({ ...createPageview(), ip: '89.160.20.129' }, await resetMetaWithMmdb())
+    expect(event!.$set).toEqual(
+        expect.objectContaining({
+            $geoip_city_name: 'Linköping',
+            $geoip_country_name: 'Sweden',
+            $geoip_country_code: 'SE',
+            $geoip_continent_name: 'Europe',
+            $geoip_continent_code: 'EU',
+            $geoip_latitude: 58.4167,
+            $geoip_longitude: 15.6167,
+            $geoip_time_zone: 'Europe/Stockholm',
+            $geoip_subdivision_1_code: 'E',
+            $geoip_subdivision_1_name: 'Östergötland County',
+        })
+    )
+    expect(event!.$set_once).toEqual(
+        expect.objectContaining({
+            $initial_geoip_city_name: 'Linköping',
+            $initial_geoip_country_name: 'Sweden',
+            $initial_geoip_country_code: 'SE',
+            $initial_geoip_continent_name: 'Europe',
+            $initial_geoip_continent_code: 'EU',
+            $initial_geoip_latitude: 58.4167,
+            $initial_geoip_longitude: 15.6167,
+            $initial_geoip_time_zone: 'Europe/Stockholm',
+            $initial_geoip_subdivision_1_code: 'E',
+            $initial_geoip_subdivision_1_name: 'Östergötland County',
+        })
+    )
+})
+
 test('error is thrown if meta.geoip is not provided', async () => {
     expect.assertions(1)
     await expect(
