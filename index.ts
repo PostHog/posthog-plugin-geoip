faa6a5a8658ea0d8a8abdac1ce2718039a4119a2
James Greenhill <fuziontech@gmail.com>
2022-02-07 19:17:48 -0800

    Use caching feature for GeoIP lookups

--- index.ts
+++ index.ts
@@ -37,7 +37,13 @@ const plugin: Plugin = {
             if (ip === '127.0.0.1') {
                 ip = '13.106.122.3' // Spoofing an Australian IP address for local development
             }
-            const response = await geoip.locate(ip)
+            var response = await cache.get(ip, false)
+            if (!response) {
+                response = await geoip.locate(ip)
+                await cache.set(ip, JSON.stringify(response))
+            } else {
+                response = JSON.parse(response)
+            }
             if (response) {
                 const location: Record<string, any> = {}
                 if (response.city) {
